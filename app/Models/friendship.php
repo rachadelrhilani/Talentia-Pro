385ce6d8eaa6a35f62b8f6324f5f9ea1ed15ee52
rachad <rachdos76@gmail.com>
2026-02-02 12:56:21 +0100

    creer le Modal friendship avec leurs relations

--- app/Models/friendship.php
+++ app/Models/friendship.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class friendship extends Model
 {
-    //
+    protected $fillable = ['sender_id','receiver_id','status'];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
