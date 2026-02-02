f7a39076124bb9e2a4567989137bd98cf8da72ad
rachad <rachdos76@gmail.com>
2026-02-02 12:54:38 +0100

    creer le modal application avec leurs relations

--- app/Models/application.php
+++ app/Models/application.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class application extends Model
 {
-    //
+    protected $fillable = ['job_offer_id','user_id','status'];
+
+    public function jobOffer()
+    {
+        return $this->belongsTo(job_offer::class);
+    }
+
+    public function candidate()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
