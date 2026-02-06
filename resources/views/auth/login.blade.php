fe54808eae7a3a7c40d8040b9f8c16a6c820c841
rachad <rachdos76@gmail.com>
2026-02-06 20:06:43 +0100

    creer un composant livewire jobdashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,16 @@
                 <label class="block text-sm text-gray-600 mb-1" for="email">E-mail ou téléphone</label>
                 <input type="email" name="email" id="email" required
                     class="w-full border border-gray-400 p-3 rounded text-black focus:border-black focus:ring-1 focus:ring-black outline-none transition duration-200">
+                @if($errors->has('email'))
+                <div class="flex items-center p-4 mb-4 text-red-800 rounded-2xl bg-red-50 border border-red-100 shadow-sm" role="alert">
+                    <svg class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+                    </svg>
+                    <div class="ml-3 text-sm font-bold">
+                        {{ $errors->first('email') }}
+                    </div>
+                </div>
+                @endif
             </div>
 
             <div class="mb-2">
@@ -40,7 +50,7 @@
 
         <div class="text-center">
             <p class="text-gray-600">
-                Nouvel utilisateur sur Talentia ? 
+                Nouvel utilisateur sur Talentia ?
                 <a href="{{ route('registerForm') }}" class="text-[#0a66c2] hover:underline font-semibold">S'inscrire</a>
             </p>
         </div>
