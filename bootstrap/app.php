562bc36b97a82f90cae19c9b0d76e2ad295506eb
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-09 21:09:42 +0100

    Input Sanitization Middleware to prevent XSS attacks by removing malicious HTML tags before request processing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // Security Headers Middleware
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+
+        // Input Sanitization Middleware
+        $middleware->append(\App\Http\Middleware\SanitizeInput::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
